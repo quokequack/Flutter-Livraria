41bf143f8fbee651b501e6b406c5461526f602de
alaquoke <alaquoke@gmail.com>
2025-04-14 19:02:17 +0000

    feat: app theme

--- lib/ui/_core/app.theme.dart
+++ lib/ui/_core/app.theme.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import "package:myapp/ui/_core/app_colors.dart";
 
+abstract class AppTheme {
+  static ThemeData appTheme = ThemeData.dark().copyWith(
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        foregroundColor: WidgetStatePropertyAll(Colors.white),
+        backgroundColor: WidgetStateColor.resolveWith((states) {
+          if (states.contains(WidgetState.pressed)) {
+            return Colors.grey;
+          } else if (states.contains(WidgetState.pressed)) {
+            return const Color.fromARGB(255, 163, 114, 85);
+          }
+          return const Color.fromARGB(255, 100, 50, 21);
+        }),
+      ),
+    ),
+  );
+}
