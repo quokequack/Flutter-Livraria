7f818c26956d8e17e9149b4dc722618127238e61
alaquoke <alaquoke@gmail.com>
2025-04-14 18:58:53 +0000

    primeiro comms

---
+++ lib/ui/_core/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color backgroundColor = Color(0xFF7D3A12);
+  static const Color mainColor = Color.fromARGB(255, 219, 163, 130);
+}
