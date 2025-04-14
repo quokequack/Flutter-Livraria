5533e9617167571999d621167c9c9ac0bb00c8af
alaquoke <alaquoke@gmail.com>
2025-04-14 19:01:04 +0000

    feat: main dart

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+import 'package:myapp/ui/_core/app.theme.dart';
+import 'package:myapp/ui/splash/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: AppTheme.appTheme, home: SplashScreen());
+  }
+}
